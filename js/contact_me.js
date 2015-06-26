289436dadd2d8af281c4361a6ffa0d5b08d2a203
U-jdm-PC\jdm <jdmotley@gmail.com>
2015-06-26 09:38:59 -0700

    changing contact solution

--- js/contact_me.js
+++ js/contact_me.js
@@ -28,11 +28,7 @@ $(function() {
             xhr.setRequestHeader("Accept", "application/json")
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
 
-            xhr.send(
-              "name=" + name +
-              "&email=" + email +
-              "&phone=" + phone +
-              "&message=" + message);
+            xhr.send("name=" + name + "&email=" + email + "&phone=" + phone + "&message=" + message);
 
             xhr.onloadend = function (res) {
               if (res.target.status === 200){
@@ -58,45 +54,7 @@ $(function() {
                 $('#contactForm').trigger("reset");
               }
             }
-    });
-
-
-
-            $.ajax({
-                url: "http://formspree.io/geekfamilyprojects@gmail.com",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+          }
         },
         filter: function() {
             return $(this).is(":visible");
